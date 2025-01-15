01bdbfed5d8d7e9c13ba9bbb3b37dafd6044df69
Dan Inon <danninon@gmail.com>
2025-01-15 17:26:45 +0200

    creater an imagePath getter, using it for in the template now

--- attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -12,5 +12,10 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   public selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath(){
+    return `assets/users/${this.selectedUser.avatar}`;
+    // return `assets/users/' + selectedUser.avatar"
+  }
 }
 
