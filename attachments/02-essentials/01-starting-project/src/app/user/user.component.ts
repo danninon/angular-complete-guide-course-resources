387146334b465c1d9738cce2abf85cb4255c7289
Dan Inon <danninon@gmail.com>
2025-01-16 11:12:48 +0200

    multiple users, removed signals

--- attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,7 +1,6 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component , Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndexGenerator = () => Math.floor(Math.random() * DUMMY_USERS.length) ;
 
 @Component({
   selector: 'app-user',
@@ -10,38 +9,15 @@ const randomIndexGenerator = () => Math.floor(Math.random() * DUMMY_USERS.length
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+
 export class UserComponent {
-   public selectedUser = signal(DUMMY_USERS[randomIndexGenerator()]);
-   public imagePath = computed(() => {
-    console.log('computed called');
-    return`assets/users/${this.selectedUser().avatar}`
-  });
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-// public selectedUser: { id: string; name: string; avatar: string } = {
-//   id: '',
-//   name: '',
-//   avatar: '',
-// };
-
-  constructor(){
-    // this.selectedUser = DUMMY_USERS[randomIndexGenerator()];
-    // this.selectedUser = signal(DUMMY_USERS[randomIndexGenerator()]);
+  get imagePath(){
+    return `assets/users/${this.avatar}`;    
   }
- 
-
-  // get imagePath(){
-    // return `assets/users/${this.selectedUser().avatar}`;
-    // return `assets/users/' + selectedUser.avatar"
-  // }
-
   onSelectUser(){
-    let newIndex = randomIndexGenerator()
-    // let oldIndex = DUMMY_USERS.indexOf(this.selectedUser);
-    // console.log(`oldIndex: ${oldIndex}`);
-    // console.log(`newIndex: ${newIndex}`);
-    // this.selectedUser = DUMMY_USERS[newIndex];
-    this.selectedUser.set(DUMMY_USERS[newIndex]);
-    // console.log('Clicked');
   }
 }
 
