72943e1aa96700d3e9a22ee72b3e1ae80e22d550
Dan Inon <danninon@gmail.com>
2025-01-16 10:52:07 +0200

    changed to usage of signals

--- attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndexGenerator = () => Math.floor(Math.random() * DUMMY_USERS.length) ;
@@ -11,7 +11,12 @@ const randomIndexGenerator = () => Math.floor(Math.random() * DUMMY_USERS.length
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  public selectedUser: { id?: string; name?: string; avatar?: string } = {};
+   public selectedUser = signal(DUMMY_USERS[randomIndexGenerator()]);
+   public imagePath = computed(() => {
+    console.log('computed called');
+    return`assets/users/${this.selectedUser().avatar}`
+  });
+
 // public selectedUser: { id: string; name: string; avatar: string } = {
 //   id: '',
 //   name: '',
@@ -19,21 +24,23 @@ export class UserComponent {
 // };
 
   constructor(){
-    this.selectedUser = DUMMY_USERS[randomIndexGenerator()];
+    // this.selectedUser = DUMMY_USERS[randomIndexGenerator()];
+    // this.selectedUser = signal(DUMMY_USERS[randomIndexGenerator()]);
   }
  
 
-  get imagePath(){
-    return `assets/users/${this.selectedUser.avatar}`;
+  // get imagePath(){
+    // return `assets/users/${this.selectedUser().avatar}`;
     // return `assets/users/' + selectedUser.avatar"
-  }
+  // }
 
   onSelectUser(){
     let newIndex = randomIndexGenerator()
     // let oldIndex = DUMMY_USERS.indexOf(this.selectedUser);
     // console.log(`oldIndex: ${oldIndex}`);
     // console.log(`newIndex: ${newIndex}`);
-    this.selectedUser = DUMMY_USERS[newIndex];
+    // this.selectedUser = DUMMY_USERS[newIndex];
+    this.selectedUser.set(DUMMY_USERS[newIndex]);
     // console.log('Clicked');
   }
 }
