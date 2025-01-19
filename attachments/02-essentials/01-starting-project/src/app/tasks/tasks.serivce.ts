01fad958f8f771b0f4b2fba34e5d3c86455ca1d9
Dan Inon <danninon@gmail.com>
2025-01-19 12:05:15 +0200

    added local storage for tasks

--- attachments/02-essentials/01-starting-project/src/app/tasks/tasks.serivce.ts
+++ attachments/02-essentials/01-starting-project/src/app/tasks/tasks.serivce.ts
@@ -1,30 +1,40 @@
-import { dummyTasks } from "../dummy-tasks";
-import { NewTaskData, Task } from "../interfaces/task.model";
-import { Injectable } from "@angular/core";
+import { dummyTasks } from '../dummy-tasks';
+import { NewTaskData, Task } from '../interfaces/task.model';
+import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
   private tasks: Task[] = dummyTasks;
 
-  constructor() {}
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
 
   addTask(taskData: NewTaskData, userId: string) {
     this.tasks.unshift({
-        id: Math.random().toString(),
-        userId: userId,
-        title: taskData.title,
-        summary: taskData.summary,
-        dueDate: taskData.date
-      });
+      id: Math.random().toString(),
+      userId: userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    });
+    this.saveTasks();
   }
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasks();
   }
 
   getTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
 
-  
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
 }
