795bc566e96974ad7a1d121cc2f4fa212c8f3291
Dan Inon <danninon@gmail.com>
2025-01-19 10:13:01 +0200

    set up two way binding to the forms elements

--- attachments/02-essentials/01-starting-project/src/app/tasks/task-form/task-form.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/tasks/task-form/task-form.component.ts
@@ -1,9 +1,10 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css'
 })
@@ -12,6 +13,9 @@ export class TaskFormComponent {
   // should have a method to handle form submission
 
   @Output() cancel = new EventEmitter<void>();
+  enteredTitle = ''
+  enteredSummary=''
+  enteredDate=''
 
   submitForm() {
       console.log('Form Submitted');
