3eef905f663acbd297e86a15b3047dc4e32aea60
Dan Inon <danninon@gmail.com>
2025-01-20 12:26:34 +0200

    swapping some css elements id and classes with "host:"

--- attachments/06-cmp-deep-dive/01-starting-project/src/app/shared/control/control.component.ts
+++ attachments/06-cmp-deep-dive/01-starting-project/src/app/shared/control/control.component.ts
@@ -6,7 +6,10 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
+   encapsulation: ViewEncapsulation.None, //because were using ng content in the control component and want the button css to work on it
+  host: { //because we changed encapsulation to none, we need to add this to the host to make the button css work on the control component
+    class: 'control'
+  }
 })
 export class ControlComponent {
   label = input.required<string>();
