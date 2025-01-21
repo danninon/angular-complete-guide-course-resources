697aa78d2a4b320597c486a38a648562f9751801
Dan Inon <danninon@gmail.com>
2025-01-21 14:35:25 +0200

    added input to a directve

--- attachments/07-directives-deep-dive/01-starting-project/src/app/safe-link.directive.ts
+++ attachments/07-directives-deep-dive/01-starting-project/src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, inject, input } from '@angular/core';
 
 //selector: any anchor element that has the appSafeLink attribute on it
 @Directive({
@@ -10,16 +10,22 @@ import { Directive } from '@angular/core';
   },
 })
 export class SafeLinkDirective {
+  queryParam = input<string>('myapp', { alias: 'appSafeLink' });
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
   constructor() {
     console.log('SafeLinkDirective is active');
   }
 
-  onConfirmLeavePage(event: MouseEvent){
+  onConfirmLeavePage(event: MouseEvent) {
     const isWantsToLeave = window.confirm('Do you want to leave the app?');
-    if (isWantsToLeave){
-        return;
-    }else{
-       event.preventDefault();
+    if (isWantsToLeave) {
+      const address = this.hostElementRef.nativeElement.href;
+      this.hostElementRef.nativeElement.href =
+        address + '?from=' + this.queryParam();
+      return;
+    } else {
+      event.preventDefault();
     }
   }
 }
