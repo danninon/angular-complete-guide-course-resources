75095b324deeff2b116aa58c2d1b6595b53af50c
Dan Inon <danninon@gmail.com>
2025-01-21 11:01:29 +0200

    basic two way bindings inner and outer components

--- attachments/06-cmp-deep-dive/19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ attachments/06-cmp-deep-dive/19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,7 +9,8 @@ import { Component } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-
+  @Input({required:true}) size!:{width:string,height:string};
+  
   onReset() {
     // ...
   }
