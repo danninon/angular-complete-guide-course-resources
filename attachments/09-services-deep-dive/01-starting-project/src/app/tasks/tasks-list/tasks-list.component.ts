32503b87ac8a1263e696832699ef1fc6808775ab
Dan Inon <danninon@gmail.com>
2025-01-21 17:49:31 +0200

    created task service and logging service, using logging inside task too

--- attachments/09-services-deep-dive/01-starting-project/src/app/tasks/tasks-list/tasks-list.component.ts
+++ attachments/09-services-deep-dive/01-starting-project/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
+import { TasksService } from '../tasks.service';
+import { Task } from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -10,9 +12,26 @@ import { TaskItemComponent } from './task-item/task-item.component';
   imports: [TaskItemComponent],
 })
 export class TasksListComponent {
-  selectedFilter = signal<string>('all');
-  tasks = [];
-
+  private tasksService = inject(TasksService);
+  
+  private selectedFilter = signal<string>('all');
+  // this.tasksService.getTasks();
+  tasks = computed(() => {
+    const allTasks = this.tasksService.getTasks()();
+    const selectedFilter = this.selectedFilter();
+    let retVal: Task[];
+    if (selectedFilter === 'all'){
+      retVal = allTasks;
+    }else if (selectedFilter === 'open'){
+      retVal = allTasks.filter(task=> task.status === 'OPEN')
+    }else if (selectedFilter === 'in-progress'){
+      retVal = allTasks.filter(task=> task.status === 'IN_PROGRESS');
+    }else{
+      retVal = allTasks.filter(task=> task.status === 'DONE');
+    }
+    return retVal;
+  })
+  
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
   }
