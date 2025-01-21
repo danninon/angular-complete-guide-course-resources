b8892796b17240ea9d98617b290dfe5d25f5ce02
Dan Inon <danninon@gmail.com>
2025-01-21 12:09:16 +0200

    made it functional, now prevents from leaving the page without asking for confirmation

--- attachments/07-directives-deep-dive/01-starting-project/src/app/safe-link.directive.ts
+++ attachments/07-directives-deep-dive/01-starting-project/src/app/safe-link.directive.ts
@@ -1,12 +1,25 @@
-import { Directive } from "@angular/core";
+import { Directive } from '@angular/core';
 
 //selector: any anchor element that has the appSafeLink attribute on it
 @Directive({
-    selector: 'a[appSafeLink]',
-    standalone: true
+  selector: 'a[appSafeLink]',
+  standalone: true,
+  host: {
+    class: 'control',
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
-export class SafeLinkDirective{
-    constructor() {
-        console.log('SafeLinkDirective is active');
+export class SafeLinkDirective {
+  constructor() {
+    console.log('SafeLinkDirective is active');
+  }
+
+  onConfirmLeavePage(event: MouseEvent){
+    const isWantsToLeave = window.confirm('Do you want to leave the app?');
+    if (isWantsToLeave){
+        return;
+    }else{
+       event.preventDefault();
     }
+  }
 }
